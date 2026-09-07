ec51d6352e80d30423f48cc27f3a8f530afa3a2e
benilsonbenito7 <benilsonbenito@gmail.com>
2026-09-07 10:51:12 -0300

    endpoint token

--- shorter/api.py
+++ shorter/api.py
@@ -8,3 +8,7 @@ sh_router = Router(tags=["Rotas do encurtador"])
 @sh_router.post('create/', response = {200: LinkOutput, 409: dict})
 def create(request, link_schema: LinkInput):
     return RedirectService.create_link(link_schema)
+
+@sh_router.get("/{token}", response = {200: None, 410: dict})
+def get_link(request, token):
+    return RedirectService.get_link(token, request)
--- shorter/services/services.py
+++ shorter/services/services.py
@@ -1,4 +1,6 @@
 from ..models import Links, Clicks
+# pyrefly: ignore [missing-import]
+from django.shortcuts import get_object_or_404, redirect
 
 class RedirectService:
 
@@ -13,3 +15,21 @@ class RedirectService:
         link.save()
         
         return 200, link
+
+    @staticmethod
+    def get_link(token, request):
+        link = get_object_or_404(Links, token=token)
+        if link.expired():
+            return 410, {'error': 'Link expirado'}
+       
+        uniques_clicks = Clicks.objects.filter(link=link).values('ip')
+        print(uniques_clicks)
+        
+        click = Clicks(
+            link=link,
+            ip=request.META.get("REMOTE_ADDR")
+        )
+        click.save()
+        
+        return redirect(link.redirect_link)
+    
